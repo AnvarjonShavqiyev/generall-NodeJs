d6519fcee026a8d489904520d141a3fe1284f6be
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2023-10-18 21:32:47 +0500

    dddd

--- api/modules/user.js
+++ api/modules/user.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    userName: String,
-    phoneNumber: String,
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    extra:{
-        payment: false,
-        score: 0,
-        levelsWord: 0,
-        levelsTest: 0
-    }
+  _id: mongoose.Schema.Types.ObjectId,
+  userName: String,
+  phoneNumber: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  extra: String,
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
